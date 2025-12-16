29bc818bc41471aa3c2a1ded547ed5e5be0620ef
luke <lfairbanks@anamosa.k12.ia.us>
2025-12-16 14:01:49 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+
+function markAsDone(todos) {
+	let i = 0;
+	while (i < todos.length) {
+		todos[i] = 'done - ' + todos[i];
+		i++;
+	}
+	return todos;
+}
+
+// expose to global for browser tests
+if (typeof window !== 'undefined') {
+	window.markAsDone = markAsDone;
+}
+
